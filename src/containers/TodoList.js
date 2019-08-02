f69daa6329be8a9acba0f3cda8f31ce65a505181
mango906 <minkb01@gmail.com>
2019-08-02 17:16:27 +0900

    [FIX] dispatch -> mapDispatchToProps

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -6,22 +6,24 @@ import { add, del } from "../actions";
 import TodoItem from "../components/TodoItem";
 import AddTodo from "../components/AddTodo";
 
+import * as types from "../actions/ActionTypes";
+
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const { todos, store } = this.props;
+    const { todos, store, add, del } = this.props;
 
     const todoItem = todos.map((todo, i) => (
-      <TodoItem key={i} {...todo} onDelete={index => store.dispatch(del(index))} />
+      <TodoItem key={i} {...todo} onDelete={index => del(index)} />
     ));
 
     return (
       <React.Fragment>
         <ul>{todoItem}</ul>
-        <AddTodo addTodo={text => store.dispatch(add(text))} />
+        <AddTodo addTodo={text => add(text)} />
       </React.Fragment>
     );
   }
@@ -33,4 +35,16 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => {
+  return {
+    add: text => {
+      dispatch({ type: types.add, text });
+    },
+    del: index => dispatch({ type: types.del, index })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
